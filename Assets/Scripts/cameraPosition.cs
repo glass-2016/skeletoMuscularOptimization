74fdade98e1298cf600dc4d6cc0069b65972153c
VPlouzeau <viviane.plouzeau@gmail.com>
2016-07-06 17:00:44 +0200

    Reworked camera.

--- Assets/Scripts/cameraPosition.cs
+++ Assets/Scripts/cameraPosition.cs
@@ -5,16 +5,25 @@ public class cameraPosition : MonoBehaviour
 {
 	private float currentAngleX = 0;
 	private float currentAngleY = 0;
+
+	private float currentPosX = 0;
+	private float currentPosY = 0;
+
 	// speed axis
-	public float xSpeed = 1f;
-	public float ySpeed = 1f;
+	public float xSpeed = 10f;
+	public float ySpeed = 10f;
 	public float zSpeed = 50f;
+	public float mouseSensibility = 0.01f;
 
 	//camera zoom cap parameters
 	public float zMax = 50f;
 	public float zMin = -50f;
 
 	private Vector3 distanceVector;
+
+	private Vector3 mouseDragBegin;
+	private Vector3 mouseDrag;
+
 
 	// Use this for initialization
 	void Start () 
@@ -43,10 +52,29 @@ public class cameraPosition : MonoBehaviour
 		{
 			// rotate camera around point
 			currentAngleX += Input.GetAxis("Mouse X") * xSpeed;
-			currentAngleY += -Input.GetAxis("Mouse Y")* ySpeed;
-			Rotate (currentAngleX, currentAngleY);
+			currentAngleY += Input.GetAxis("Mouse Y")* ySpeed;
+			//Rotate (currentAngleX, currentAngleY);
+			//Reversed because it works better
+			transform.localEulerAngles = new Vector3(currentAngleY, currentAngleX, 0);
+
 		}
-		// translate camera
+
+		//translate camera
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			mouseDragBegin = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButton(0))
+		{
+			mouseDrag = Input.mousePosition - mouseDragBegin;
+			transform.Translate (mouseDrag.x*mouseSensibility,mouseDrag.y*mouseSensibility,0);
+			//transform.Translate (currentPosY,currentPosX,0);
+			mouseDragBegin = Input.mousePosition;
+			
+		}
+
+	/*	// translate camera
 		if (Input.GetKey (KeyCode.LeftArrow))
 			transform.Translate(Vector3.left * 0.1f, Space.Self);
 		if (Input.GetKey (KeyCode.RightArrow))
@@ -54,7 +82,7 @@ public class cameraPosition : MonoBehaviour
 		if (Input.GetKey (KeyCode.UpArrow))
 			transform.Translate(Vector3.forward * 0.1f, Space.Self);
 		if (Input.GetKey (KeyCode.DownArrow))
-			transform.Translate(-Vector3.forward * 0.1f, Space.Self);
+			transform.Translate(-Vector3.forward * 0.1f, Space.Self); */
 		
 		// zoom
 		float zoomAmount = Input.GetAxis ("Mouse ScrollWheel") * zSpeed;
@@ -69,12 +97,16 @@ public class cameraPosition : MonoBehaviour
 		}
 		if (zPos > zMax) 
 		{
-			//transform.Translate (Vector3.forward, zMax);
+			Vector3 camPos = transform.position;
+			camPos.z = zMax;
+			transform.position = camPos;
 		}
 
 		if (zPos < zMin) 
 		{
-			//transform.Translate (Vector3.forward, zMin);
+			Vector3 camPos = transform.position;
+			camPos.z = zMin;
+			transform.position = camPos;
 		}
 
 	}
