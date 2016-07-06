adbc9ace1a0e5082de5687b38b805d5c8e7f898b
Clement Barbisan <c.barbisan@free.fr>
2016-07-06 14:09:30 +0200

    first attempt to implement configurable joint

--- Assets/Scripts/musclesController.cs
+++ Assets/Scripts/musclesController.cs
@@ -8,6 +8,7 @@ public class musclesController : MonoBehaviour
 	private ConfigurableJoint joint = null;
 	// anchor prefab for joint anchors only for debug
 	public GameObject anchorPrefab;
+	private Rigidbody rb;
 	public bool debug = false;
 	public List<muscle> listMuscles;
 	private GameObject[] anchors;
@@ -17,11 +18,12 @@ public class musclesController : MonoBehaviour
 	{
 		anchors = new GameObject[2];
 		listMuscles = new List<muscle> ();
+		rb = GetComponent<Rigidbody> ();
 	}
 
 	public void addDirection(vec3i dir)
 	{
-		joint.axis += new Vector3 (dir.x, dir.y, dir.z);
+		joint.axis += new Vector3 (dir.z, dir.x, dir.y);
 		joint.targetRotation = Quaternion.Euler(joint.axis);
 	}
 
@@ -36,7 +38,7 @@ public class musclesController : MonoBehaviour
 		if (debug)
 		{
 			anchors[0] = Instantiate (anchorPrefab, joint.anchor, Quaternion.identity) as GameObject;
-			anchors[1] = Instantiate (anchorPrefab, joint.connectedAnchor, Quaternion.identity) as GameObject;
+//			anchors[1] = Instantiate (anchorPrefab, joint.connectedAnchor, Quaternion.identity) as GameObject;
 		}
 		joint.xMotion = ConfigurableJointMotion.Limited;
 		joint.yMotion = ConfigurableJointMotion.Limited;
@@ -69,6 +71,7 @@ public class musclesController : MonoBehaviour
 	public void setForce(float force)
 	{
 		joint.targetVelocity += joint.axis * force;
+		rb.angularVelocity = joint.targetVelocity * Time.deltaTime;
 	}
 
 	// Update is called once per frame
@@ -76,8 +79,8 @@ public class musclesController : MonoBehaviour
 	{
 		if (debug && joint && joint.connectedBody)
 		{
-			anchors[0].transform.position = (joint.connectedBody.transform.position - transform.position) / 2.0f;
-			anchors[1].transform.position = -(joint.connectedBody.transform.position - transform.position) / 2.0f;
+			anchors[0].transform.position = transform.position + (joint.connectedBody.transform.position - transform.position) / 2.0f;
+//			anchors[1].transform.position = -(joint.connectedBody.transform.position - transform.position) / 2.0f;
 		}
 	}
 }
